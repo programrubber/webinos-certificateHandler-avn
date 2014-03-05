4cebf692f40c1f9915b5795f5e47e740203765e3
sekwon <sekwon.jang@obigo.com>
2014-03-05 13:07:49 +0900

    added script for cross compile env

---
+++ cross_compile_npm.py
@@ -0,0 +1,45 @@
+#!/usr/bin/env python
+
+import os
+import sys
+import subprocess
+
+if len(sys.argv) < 2:
+	print sys.argv[0] + " " + "<npm command>" + " " + "[node package module name]"
+	sys.exit()
+
+PATH_CROSS_COMPILER = "/home/douyaji/backfire/staging_dir/toolchain-arm_v6k_gcc-4.6-linaro_uClibc-0.9.33.2_eabi/bin"
+
+print "adding cross compiler path"
+PATH_OLD = os.environ['PATH']
+print "old path = %(PATH_OLD)s"%locals()
+PATH_NEW = PATH_OLD + ':' + PATH_CROSS_COMPILER
+print "new path = %(PATH_NEW)s"%locals()
+os.environ['PATH'] = PATH_NEW
+
+os.environ['AR'] = "arm-openwrt-linux-ar"
+print "AR=" + os.environ['AR']
+os.environ['CC'] = "arm-openwrt-linux-gcc"
+print "CC=" + os.environ['CC']
+os.environ['CXX'] = "arm-openwrt-linux-g++"
+print "CXX=" + os.environ['CXX']
+os.environ['LINK'] = "arm-openwrt-linux-g++"
+print "LINK=" + os.environ['LINK']
+os.environ['STAGING_DIR'] = "/home/douyaji/backfire/staging_dir/"
+print "LINK=" + os.environ['STAGING_DIR']
+
+print "============================================="
+print "Cross Compiling Node Package Module"
+print "============================================="
+CMD_NPM = "npm"
+CMD_NPM += " " + sys.argv[1]
+CMD_NPM += " " + "--arch=arm"
+CMD_NPM += " "
+
+for arg in sys.argv[2:]:
+	if arg != sys.argv[0]:
+		CMD_NPM += arg + " "		
+
+print CMD_NPM
+subprocess.call(CMD_NPM, shell=True)
+
